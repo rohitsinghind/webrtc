cd3dd8f8c3d185cace384a1e5d493dbab77039a3
rohitsinghind <rohitsinghind22@gmail.com>
2024-05-27 09:55:48 +0530

    code push

---
+++ server.js
@@ -0,0 +1,44 @@
+
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+app.use(cors());
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+  cors: { origin: "http://127.0.0.1/:5173" },
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected");
+
+  socket.on("message", (message) => {
+    socket.broadcast.emit("message", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Disconnected");
+  });
+});
+
+
+
+function error(err, req, res, next) {
+  // log it
+  if (!test) console.error(err.stack);
+
+  // respond with 500 "Internal Server Error".                                                 
+  res.status(500);
+  res.send("Internal Server Error");
+}
+app.use(error);
+
+app.use(express.static(path.join(__dirname, "./client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/dist/index.html"));
+});
+
+server.listen(3000, () => {
+  console.log("listening on Port 3000");
+});
