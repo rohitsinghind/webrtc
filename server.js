407ea021d362c22cf9abf197144da01c54f3264d
rohitsinghind <rohitsinghind22@gmail.com>
2024-05-27 10:11:38 +0530

    updated path

--- server.js
+++ server.js
@@ -33,10 +33,10 @@ function error(err, req, res, next) {
 }
 app.use(error);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "/client/dist/index.html"));
+  res.sendFile(path.resolve(__dirname, "client/dist/index.html"));
 });
 
 server.listen(3000, () => {
